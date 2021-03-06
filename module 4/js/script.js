7d432b1350a88b54e07fb3c43bbec193314d25e0
kumar harsh <rockingharsh305@gmail.com>
2021-03-06 22:05:09 +0530

    done

---
+++ module 4/js/script.js
@@ -0,0 +1,16 @@
+var name=new Araay();
+names[0]="yaakov";
+names[1]="john";
+names[2]="jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[0]="jim";
+
+for(var i=0;i<names.lenth;i++)
+{
+    console.log(names[i]);
+}
